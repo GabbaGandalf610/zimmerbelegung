86c3d900c7138596364fa11f393d2033d0431649
Stefan Feilmeier <Stefan Feilmeier@9f5d0959-232f-0424-817a-97dd6493bbc2>
2012-05-01 18:36:24 +0000

    Administrationsoberfläche (Update + Delete) für Buchungen nach dem ZK MVVM-Konzept: http://localhost:8080/Zimmerbelegung/adminBuchung.zul Dokumentation: http://books.zkoss.org/wiki/ZK_Developer%27s_Reference/MVVM

--- src/de/hdu/zimmerbelegung/ctrl/AdminBuchungCtrl.java
+++ src/de/hdu/zimmerbelegung/ctrl/AdminBuchungCtrl.java
@@ -1,83 +1,52 @@
 package de.hdu.zimmerbelegung.ctrl;
 
-import org.zkoss.zul.Label;
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.event.SelectEvent;
-import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Listen;
-import org.zkoss.zk.ui.select.annotation.Wire;
+
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zul.ListModel;
 import org.zkoss.zul.ListModelList;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Listcell;
-import org.zkoss.zul.Listitem;
-import org.zkoss.zul.ListitemRenderer;
-import org.zkoss.zul.Textbox;
 
 import de.hdu.zimmerbelegung.manager.AdminBuchungManager;
 import de.hdu.zimmerbelegung.model.Buchung;
 import de.hdu.zimmerbelegung.service.ServiceLocator;
 
-public class AdminBuchungCtrl extends SelectorComposer<Component> {
-	private static final long serialVersionUID = -8889573485448848014L;
+public class AdminBuchungCtrl {
+	// the search condition
+	String filter;
+	// the search result
+	ListModelList<Buchung> items;
+	// the selected item
+	Buchung selected;
 
-	@Wire
-	private Listbox adminBuchungList;
-
-	private Buchung buchung;
-	@Wire
-	private Label id;
-	@Wire
-	private Textbox datum;
-
-	@Override
-	public void doAfterCompose(Component comp) throws Exception {
-		super.doAfterCompose(comp);
-		AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-		adminBuchungList.setModel(new ListModelList<Buchung>(manager.getAll()));
-		adminBuchungList.setItemRenderer(new ListitemRenderer<Buchung>() {
-			public void render(Listitem item, Buchung buchung, int index)
-					throws Exception {
-				new Listcell(String.valueOf(buchung.getId())).setParent(item);
-				new Listcell(String.valueOf(buchung.getDatum()))
-						.setParent(item);
-			}
-		});
+	public void setSelected(Buchung selected) {
+		this.selected = selected;
 	}
-
-	@Listen("onSelect = #adminBuchungList")
-	public void getBuchung(SelectEvent<Listbox, Buchung> e) {
-		buchung = e.getSelectedObjects().iterator().next();
-		id.setValue(String.valueOf(buchung.getId()));
-		datum.setValue(String.valueOf(buchung.getDatum()));
+	public Buchung getSelected() {
+		return selected;
 	}
-
-	@Listen("onClick=#adminBuchungUpdate")
-	public void adminBuchungUpdate() {
-		// TODO: buchung.setDatum(Date.parse((datum.getValue()));
-		AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-		manager.update(buchung);
+	public ListModel<Buchung> getItems() {
+		if (items == null) {
+			// Liste initialisieren
+			items = new ListModelList<Buchung>();
+			AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
+			items.addAll(manager.getAll());
+		}
+		return items;
 	}
 	
-	@Listen("onClick=#adminBuchungDelete")
-	public void adminBuchungDelete() {
-		// TODO: buchung.setDatum(Date.parse((datum.getValue()));
+	@Command
+	@NotifyChange({ "selected", "items" })
+	public void doUpdate() {
 		AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-		manager.delete(buchung);
-		adminBuchungList.setModel(new ListModelList<Buchung>(manager.getAll()));
+		manager.update(selected);
 	}
-
-	// public void onClick$addBuchung() {
-	// if (null == buchung) {
-	// buchung = new Buchung();
-	// }
-	// AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-	// manager.add(buchung);
-	// buchung = null;
-	// }
-	//
-	// public void onClick$deleteBuchung() {
-	// AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-	// manager.delete(buchung);
-	// buchung = null;
-	// }
+	
+	@Command
+	@NotifyChange({ "selected", "items" })
+	public void doDelete() {
+		AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
+		manager.delete(selected);
+		items.remove(selected);
+		selected = null;
+	}
 }
--- src/de/hdu/zimmerbelegung/model/Zimmer.java
+++ src/de/hdu/zimmerbelegung/model/Zimmer.java
@@ -56,6 +56,9 @@ public class Zimmer {
 
 	}
 
+	public String toString() {
+		return this.name + " (" + this.kurzbeschreibung + ")";
+	}
 
 
 }
