d895c503c64fbd3448c7feeca538a9e528464ab5
wagnerfranz12@googlemail.com <wagnerfranz12@googlemail.com@9f5d0959-232f-0424-817a-97dd6493bbc2>
2012-05-02 20:33:05 +0000

    Ändern der Controller für Gast und TagStatus, Funktion "doInsert" für Gast und TagStatus. Soweit lauffähig, aber Fehler bei TagStatus, wenn Tabelle nicht leer, muss ich nochmal testen! In der index.zul habe ich den Tag "iframe" benutzt, um auf die admin*.zul zu verweisen. Daten werden gleich zu beginn geladen, auch ohne "apply" im "tabpanel"-Tag!

--- trunk/src/de/hdu/zimmerbelegung/ctrl/AdminTagStatusCtrl.java
+++ trunk/src/de/hdu/zimmerbelegung/ctrl/AdminTagStatusCtrl.java
@@ -1,19 +1,13 @@
 package de.hdu.zimmerbelegung.ctrl;
 
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.select.annotation.Wire;
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zk.ui.util.Composer;
 import org.zkoss.zk.ui.util.GenericForwardComposer;
+import org.zkoss.zul.ListModel;
 import org.zkoss.zul.ListModelList;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Listcell;
-import org.zkoss.zul.Listitem;
-import org.zkoss.zul.ListitemRenderer;
 
-import de.hdu.zimmerbelegung.manager.AdminGastManager;
-import de.hdu.zimmerbelegung.manager.AdminManager;
 import de.hdu.zimmerbelegung.manager.AdminTagStatusManager;
-import de.hdu.zimmerbelegung.model.Gast;
 import de.hdu.zimmerbelegung.model.TagStatus;
 import de.hdu.zimmerbelegung.service.ServiceLocator;
 
@@ -21,61 +15,52 @@ import de.hdu.zimmerbelegung.service.ServiceLocator;
 public class AdminTagStatusCtrl extends GenericForwardComposer implements Composer  {
 
 	
-	@Wire
-	private Listbox adminTagStatusList;
-	
-    @Override
-    public void doAfterCompose(Component comp) throws Exception {
-        super.doAfterCompose(comp);
-        AdminTagStatusManager manager = ServiceLocator.getAdminTagStatusManager();
-        adminTagStatusList.setModel(new ListModelList<TagStatus>(manager.getAllTagStatus()));
-        adminTagStatusList.setItemRenderer(new ListitemRenderer<TagStatus>() {
-        	public void render(Listitem item, TagStatus tagStatus, int index)
-    				throws Exception {
-    	    	new Listcell(String.valueOf(tagStatus.getId())).setParent(item);
-    	    	new Listcell(String.valueOf(tagStatus.getName())).setParent(item);
-    		}	  	
-        });
-    }
-	
-	
-	
-	
-//	private TagStatus tagstatus;
-//	
-//	public TagStatus getTagstatus() {
-//		return tagstatus;
-//	}
-//
-//	public void setTagstatus(TagStatus tagstatus) {
-//		this.tagstatus = tagstatus;
-//	}
-//
-//	public void onClick$addTagStatus() {
-//		if (null == tagstatus) {
-//			tagstatus = new TagStatus();
-//		}
-//		AdminManager manager = ServiceLocator.getAdminManager();
-//		manager.add(tagstatus);
-//		tagstatus = null;
-//	}
-//
-//	public void onClick$updateTagStatus() {
-//		AdminManager manager = ServiceLocator.getAdminManager();
-//		manager.update(tagstatus);
-//		tagstatus = null;
-//	}
-//
-//	public void onClick$deleteTagStatus() {
-//		AdminManager manager = ServiceLocator.getAdminManager();
-//		manager.delete(tagstatus);
-//		tagstatus = null;
-//	}
-//	
-//	@Override
-//	public void doAfterCompose(Component comp) throws Exception {
-//		super.doAfterCompose(comp);
-//		getAllTagStatus();
-//	}
+	// the search condition
+	String filter;
+	// the search result
+	ListModelList<TagStatus> items;
+	// the selected item
+	TagStatus selected;
+
+	public void setSelected(TagStatus selected) {
+		this.selected = selected;
+	}
+
+	public TagStatus getSelected() {
+		return selected;
+	}
+
+	public ListModel<TagStatus> getItems() {
+		if (items == null) {
+			// Liste initialisieren
+			items = new ListModelList<TagStatus>();
+			AdminTagStatusManager manager = ServiceLocator.getAdminTagStatusManager();
+			items.addAll(manager.getAllTagStatus());
+		}
+		return items;
+	}
+
+	@Command
+	public void doInsert() {
+		TagStatus neuerTagStatus = new TagStatus();
+		items.add(neuerTagStatus);
+		selected = neuerTagStatus;
+	}
+
+	@Command
+	@NotifyChange({ "selected", "items" })
+	public void doUpdate() {
+		AdminTagStatusManager manager = ServiceLocator.getAdminTagStatusManager();
+		manager.update(selected);
+	}
+
+	@Command
+	@NotifyChange({ "selected", "items" })
+	public void doDelete() {
+		AdminTagStatusManager manager = ServiceLocator.getAdminTagStatusManager();
+		manager.delete(selected);
+		items.remove(selected);
+		selected = null;
+	}
 	
 }
