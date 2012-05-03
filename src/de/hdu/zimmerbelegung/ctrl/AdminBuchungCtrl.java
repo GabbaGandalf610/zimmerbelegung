9ea243982923f5d02b051bd8cd07f63f3fa3d5af
Stefan Feilmeier <Stefan Feilmeier@9f5d0959-232f-0424-817a-97dd6493bbc2>
2012-05-03 22:08:59 +0000

    Buchungen anlegen und ändern (inkl. Gast + Zimmer) in der Administrationsoberfläche. Ich verzichte jetzt auf die Manager-Klassen und greife über den ServiceLocator direkt auf die DAOs zu. Ich glaube, damit machen wir uns das Leben deutlich einfacher...

--- src/de/hdu/zimmerbelegung/ctrl/AdminBuchungCtrl.java
+++ src/de/hdu/zimmerbelegung/ctrl/AdminBuchungCtrl.java
@@ -3,50 +3,81 @@ package de.hdu.zimmerbelegung.ctrl;
 
 import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.bind.annotation.NotifyChangeDisabled;
 import org.zkoss.zul.ListModel;
 import org.zkoss.zul.ListModelList;
 
+import de.hdu.zimmerbelegung.dao.GastDao;
+import de.hdu.zimmerbelegung.dao.ZimmerDao;
 import de.hdu.zimmerbelegung.manager.AdminBuchungManager;
 import de.hdu.zimmerbelegung.model.Buchung;
+import de.hdu.zimmerbelegung.model.Gast;
+import de.hdu.zimmerbelegung.model.Zimmer;
 import de.hdu.zimmerbelegung.service.ServiceLocator;
 
 public class AdminBuchungCtrl {
-	// the search condition
-	String filter;
-	// the search result
-	ListModelList<Buchung> items;
-	// the selected item
-	Buchung selected;
+	ListModelList<Buchung> buchungList;
+	ListModelList<Gast> gastList;
+	ListModelList<Zimmer> zimmerList;
+	Buchung buchungSelected;
 
-	public void setSelected(Buchung selected) {
-		this.selected = selected;
+	public void setBuchungSelected(Buchung selected) {
+		this.buchungSelected = selected;
 	}
-	public Buchung getSelected() {
-		return selected;
+	public Buchung getBuchungSelected() {
+		return buchungSelected;
 	}
-	public ListModel<Buchung> getItems() {
-		if (items == null) {
+	public ListModel<Buchung> getBuchungList() {
+		if (buchungList == null) {
 			// Liste initialisieren
-			items = new ListModelList<Buchung>();
+			buchungList = new ListModelList<Buchung>();
 			AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-			items.addAll(manager.getAll());
+			buchungList.addAll(manager.getAll());
 		}
-		return items;
+		return buchungList;
+	}
+    @NotifyChangeDisabled
+	public ListModel<Gast> getGastList() {
+		if (gastList == null) {
+			// Liste initialisieren
+			gastList = new ListModelList<Gast>();
+			GastDao gastDao = ServiceLocator.getGastDao();
+			gastList.addAll(gastDao.getAll());
+		}
+		return gastList;
+	}
+    @NotifyChangeDisabled
+	public ListModel<Zimmer> getZimmerList() {
+		if (zimmerList == null) {
+			// Liste initialisieren
+			zimmerList = new ListModelList<Zimmer>();
+			ZimmerDao zimmerDao = ServiceLocator.getZimmerDao();
+			zimmerList.addAll(zimmerDao.getAll());
+		}
+		return zimmerList;
 	}
 	
 	@Command
-	@NotifyChange({ "selected", "items" })
-	public void doUpdate() {
+	@NotifyChange({ "buchungSelected", "buchungList" })
+	public void doNew() {
+		buchungSelected = new Buchung();
+	}
+	
+	@Command
+	@NotifyChange({ "buchungSelected", "buchungList" })
+	public void doSave() {
 		AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-		manager.update(selected);
+		manager.update(buchungSelected);
+		if (!buchungList.contains(buchungSelected))
+			buchungList.add(buchungSelected);
 	}
 	
 	@Command
-	@NotifyChange({ "selected", "items" })
+	@NotifyChange({ "buchungSelected", "buchungList" })
 	public void doDelete() {
 		AdminBuchungManager manager = ServiceLocator.getAdminBuchungManager();
-		manager.delete(selected);
-		items.remove(selected);
-		selected = null;
+		manager.delete(buchungSelected);
+		buchungList.remove(buchungSelected);
+		buchungSelected = null;
 	}
 }
