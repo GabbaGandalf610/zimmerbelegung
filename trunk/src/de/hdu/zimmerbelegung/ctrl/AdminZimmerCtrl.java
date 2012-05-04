5d56573da7ea1781b76004bfdea56ea576c4970d
Roland.78@gmx.net <Roland.78@gmx.net@9f5d0959-232f-0424-817a-97dd6493bbc2>
2012-05-04 13:33:47 +0000

    ersetzte files wurden gelöscht; diverse Änderungen... Mgr gelöscht, Zugriff über Dao objekt, zimmer files an namenskonv. angepasst

--- trunk/src/de/hdu/zimmerbelegung/ctrl/AdminZimmerCtrl.java
+++ trunk/src/de/hdu/zimmerbelegung/ctrl/AdminZimmerCtrl.java
@@ -1,86 +1,101 @@
 package de.hdu.zimmerbelegung.ctrl;
 
-import org.zkoss.zk.ui.Component;
-import org.zkoss.zk.ui.select.SelectorComposer;
-import org.zkoss.zk.ui.select.annotation.Wire;
-
-
+import org.zkoss.bind.annotation.Command;
+import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zul.ListModel;
 import org.zkoss.zul.ListModelList;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Listcell;
-import org.zkoss.zul.Listitem;
-import org.zkoss.zul.ListitemRenderer;
-import de.hdu.zimmerbelegung.manager.AdminZimmerManager;
-import de.hdu.zimmerbelegung.model.Zimmer;
 
+import de.hdu.zimmerbelegung.dao.ZimmerDao;
+import de.hdu.zimmerbelegung.model.Zimmer;
 import de.hdu.zimmerbelegung.service.ServiceLocator;
 
-public class AdminZimmerCtrl extends SelectorComposer<Component> {
-	
+public class AdminZimmerCtrl {
 
+	ListModelList<Zimmer> items;
+	Zimmer selected;
+	private boolean addingNewItemMode = false;
+	private boolean showSpecialButtons = true;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5492437630185952581L;
-	@Wire
-	private Listbox adminZimmerList;
-
-	@Override
-	public void doAfterCompose(Component comp) throws Exception {
-		super.doAfterCompose(comp);
-//		AdminZimmerManager manager = ServiceLocator.getAdminZimmerManager();
-//		adminZimmerList.setModel(new ListModelList<Zimmer>(manager.getAllZimmer()));
-		adminZimmerList.setItemRenderer(new ListitemRenderer<Zimmer>() {
-        	public void render(Listitem item, Zimmer zimmer, int index)
-    				throws Exception {
-    	    	new Listcell(String.valueOf(zimmer.getId())).setParent(item);
-//    	    	new Listcell(String.valueOf(zimmer.getName())).setParent(item);
-//    	    	new Listcell(String.valueOf(zimmer.getKurzbeschreibung())).setParent(item);
-    		}
-		});
+	@Init
+	public ListModel<Zimmer> getItems() {
+		if (items == null) {
+			items = new ListModelList<Zimmer>();
+			ZimmerDao zimmerDao = ServiceLocator.getZimmerDao();
+			items.addAll(zimmerDao.getAll());
+		}
+		return items;
 	}
 
-	private Zimmer zimmer;
-
-	public Zimmer getZimmer() {
-		return zimmer;
+	@Command("create")
+	@NotifyChange({ "selected", "addingNewItemMode", "showSpecialButtons" })
+	public void createNewZimmer() {
+		setSelected(getInitializedZimmer());
+		setAddingNewItemMode(true);
+		setShowSpecialButtons(false);
 	}
 
-	public void setZimmer(Zimmer zimmer) {
-		this.zimmer = zimmer;
+	@Command("selected")
+	@NotifyChange("selected")
+	public void selectedEditingZimmer() {
 	}
 
-	// public List<Zimmer> getAllZimmer() {
-	// AdminZimmerManager manager = ServiceLocator.getAdminZimmerManager();
-	// return manager.getAllZimmer();
-	// }
+	@Command("save")
+	@NotifyChange({ "items", "addingNewItemMode", "showSpecialButtons" })
+	public void addNewZimmer() {
+		ZimmerDao zimmerDao = ServiceLocator.getZimmerDao();
+		zimmerDao.saveOrUpdate(selected);
+		if (addingNewItemMode == true) {
+			items.add(selected);
+			setAddingNewItemMode(false);
+			setShowSpecialButtons(true);
+		}
+	}
 
-	// @Override
-	// public void doAfterCompose(Component comp) throws Exception {
-	// super.doAfterCompose(comp);
-	// getAllZimmer();
-	// }
-	//
-	// public void onClick$addZimmer() {
-	// if (null == zimmer) {
-	// zimmer = new Zimmer();
-	// }
-	// AdminZimmerManager manager = ServiceLocator.getAdminZimmerManager();
-	// manager.add(zimmer);
-	// zimmer = null;
-	// }
-	//
-	// public void onClick$updateZimmer() {
-	// AdminZimmerManager manager = ServiceLocator.getAdminZimmerManager();
-	// manager.update(zimmer);
-	// zimmer = null;
-	// }
-	//
-	// public void onClick$deleteZimmer() {
-	// AdminZimmerManager manager = ServiceLocator.getAdminZimmerManager();
-	// manager.delete(zimmer);
-	// zimmer = null;
-	// }
+	@Command("delete")
+	@NotifyChange({ "selected", "items", "showSpecialButtons"  })
+	public void removeSelected() {
+		ZimmerDao zimmerDao = ServiceLocator.getZimmerDao();
+		zimmerDao.delete(selected);
+		items.remove(selected);
+		selected = null;
+	}
+
+	@Command("cancel")
+	@NotifyChange({ "selected", "items", "addingNewItemMode",
+			"showSpecialButtons" })
+	public void cancelSelected() {
+		setAddingNewItemMode(false);
+		setShowSpecialButtons(true);
+		selected = null;
+	}
+
+	public void setSelected(Zimmer selected) {
+		this.selected = selected;
+	}
+
+	public Zimmer getSelected() {
+		return selected;
+	}
+
+	public boolean isAddingNewItemMode() {
+		return addingNewItemMode;
+	}
+
+	public void setAddingNewItemMode(boolean addingNewItemMode) {
+		this.addingNewItemMode = addingNewItemMode;
+	}
+
+	public boolean isShowSpecialButtons() {
+		return showSpecialButtons;
+	}
+
+	public void setShowSpecialButtons(boolean showSpecialButtons) {
+		this.showSpecialButtons = showSpecialButtons;
+	}
+
+	private Zimmer getInitializedZimmer() {
+		return new Zimmer(0, "", 0);
+	}
 
 }
