6f679b70c987886b950a0fb633cc994e4fca4b58
Roland.78 <Roland.78@gmx.net>
2012-05-06 11:05:21 +0000

    Layout Änderungen + ZimmerCtrl den anderen Ctrls angepasst.

--- src/de/hdu/zimmerbelegung/ctrl/AdminZimmerCtrl.java
+++ src/de/hdu/zimmerbelegung/ctrl/AdminZimmerCtrl.java
@@ -1,7 +1,6 @@
 package de.hdu.zimmerbelegung.ctrl;
 
 import org.zkoss.bind.annotation.Command;
-import org.zkoss.bind.annotation.Init;
 import org.zkoss.bind.annotation.NotifyChange;
 import org.zkoss.zul.ListModel;
 import org.zkoss.zul.ListModelList;
@@ -11,91 +10,48 @@ import de.hdu.zimmerbelegung.model.Zimmer;
 import de.hdu.zimmerbelegung.service.ServiceLocator;
 
 public class AdminZimmerCtrl {
-
-	ListModelList<Zimmer> items;
-	Zimmer selected;
-	private boolean addingNewItemMode = false;
-	private boolean showSpecialButtons = true;
-
-	@Init
-	public ListModel<Zimmer> getItems() {
-		if (items == null) {
-			items = new ListModelList<Zimmer>();
+	ListModelList<Zimmer> zimmerList;
+	Zimmer zimmerSelected;
+	
+	public void setZimmerSelected(Zimmer  zimmerSelected){
+		this.zimmerSelected = zimmerSelected;
+	}
+	
+	public Zimmer getZimmerSelected(){
+		return zimmerSelected;
+	}
+	
+	public ListModel<Zimmer> getItems(){
+		if(zimmerList == null){
+			zimmerList = new ListModelList<Zimmer>();
 			ZimmerDao zimmerDao = ServiceLocator.getZimmerDao();
-			items.addAll(zimmerDao.getAll());
+			zimmerList.addAll(zimmerDao.getAll());
 		}
-		return items;
+		return zimmerList;
 	}
 
-	@Command("create")
-	@NotifyChange({ "selected", "addingNewItemMode", "showSpecialButtons" })
-	public void createNewZimmer() {
-		setSelected(getInitializedZimmer());
-		setAddingNewItemMode(true);
-		setShowSpecialButtons(false);
+	@Command
+	@NotifyChange({ "zimmerSelected", "zimmerList" })
+	public void doNew(){
+		zimmerSelected = new Zimmer();
 	}
-
-	@Command("selected")
-	@NotifyChange("selected")
-	public void selectedEditingZimmer() {
-	}
-
-	@Command("save")
-	@NotifyChange({ "items", "addingNewItemMode", "showSpecialButtons" })
-	public void addNewZimmer() {
-		ZimmerDao zimmerDao = ServiceLocator.getZimmerDao();
-		zimmerDao.saveOrUpdate(selected);
-		if (addingNewItemMode == true) {
-			items.add(selected);
-			setAddingNewItemMode(false);
-			setShowSpecialButtons(true);
+	
+	@Command
+	@NotifyChange({ "zimmerSelected", "zimmerList" })
+	public void doSave(){
+		ZimmerDao zimmerdao = ServiceLocator.getZimmerDao();
+		zimmerdao.saveOrUpdate(zimmerSelected);
+		if (!zimmerList.contains(zimmerSelected)){
+			zimmerList.add(zimmerSelected);
 		}
 	}
-
-	@Command("delete")
-	@NotifyChange({ "selected", "items", "showSpecialButtons"  })
-	public void removeSelected() {
+	
+	@Command
+	@NotifyChange({ "zimmerSelected", "zimmerList" })
+	public void doDelete(){
 		ZimmerDao zimmerDao = ServiceLocator.getZimmerDao();
-		zimmerDao.delete(selected);
-		items.remove(selected);
-		selected = null;
+		zimmerDao.delete(zimmerSelected);
+		zimmerList.remove(zimmerSelected);
+		zimmerSelected = null;
 	}
-
-	@Command("cancel")
-	@NotifyChange({ "selected", "items", "addingNewItemMode",
-			"showSpecialButtons" })
-	public void cancelSelected() {
-		setAddingNewItemMode(false);
-		setShowSpecialButtons(true);
-		selected = null;
-	}
-
-	public void setSelected(Zimmer selected) {
-		this.selected = selected;
-	}
-
-	public Zimmer getSelected() {
-		return selected;
-	}
-
-	public boolean isAddingNewItemMode() {
-		return addingNewItemMode;
-	}
-
-	public void setAddingNewItemMode(boolean addingNewItemMode) {
-		this.addingNewItemMode = addingNewItemMode;
-	}
-
-	public boolean isShowSpecialButtons() {
-		return showSpecialButtons;
-	}
-
-	public void setShowSpecialButtons(boolean showSpecialButtons) {
-		this.showSpecialButtons = showSpecialButtons;
-	}
-
-	private Zimmer getInitializedZimmer() {
-		return new Zimmer(0, "", 0);
-	}
-
 }
