b73743e034a1021c97f6312de8a2636443c5150b
Stefan Feilmeier <Stefan Feilmeier@9f5d0959-232f-0424-817a-97dd6493bbc2>
2012-05-15 21:25:20 +0000

    Zusammenfassung der Reservierung/ Buchung + kleinere Aufräumarbeiten

--- trunk/src/de/hdu/zimmerbelegung/ctrl/IndexCtrl.java
+++ trunk/src/de/hdu/zimmerbelegung/ctrl/IndexCtrl.java
@@ -2,36 +2,71 @@ package de.hdu.zimmerbelegung.ctrl;
 
 import org.joda.time.LocalDate;
 import org.zkoss.bind.annotation.Command;
-import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.bind.annotation.DependsOn;
 import org.zkoss.zul.ListModel;
 import org.zkoss.zul.ListModelList;
 import org.zkoss.zul.SimpleDateConstraint;
 
 import de.hdu.zimmerbelegung.dao.GastDao;
 import de.hdu.zimmerbelegung.dto.ZimmerZeitraumBelegungDto;
+import de.hdu.zimmerbelegung.helper.Status;
 import de.hdu.zimmerbelegung.helper.ZimmerZeitraumBelegung;
 import de.hdu.zimmerbelegung.model.Gast;
 import de.hdu.zimmerbelegung.service.ServiceLocator;
 
 public class IndexCtrl {
-	ListModelList<ZimmerZeitraumBelegung> zimmerZeitraumBelegungList;
-	ZimmerZeitraumBelegung zimmerZeitraumBelegungSelected;
+	LocalDate datumBis = new LocalDate();
+	LocalDate datumVon = new LocalDate();
 	ListModelList<Gast> gastList;
 	Gast gastSelected;
 	String gastSuche;
 
-	public String getGastSuche() {
-		return gastSuche;
-	}
-
-	public void setGastSuche(String gastSuche) {
-		this.gastSuche = gastSuche;
-	}
-
-	LocalDate datumVon = new LocalDate();
-	LocalDate datumBis = new LocalDate();
 	private final int MAX_BUCHUNGSLAENGE = 1;
 
+	ListModelList<ZimmerZeitraumBelegung> zimmerZeitraumBelegungList;
+
+	ZimmerZeitraumBelegung zimmerZeitraumBelegungSelected;
+
+	@Command
+	public void doDelete() {
+		GastDao gastDao = ServiceLocator.getGastDao();
+		gastDao.delete(gastSelected);
+		gastList.remove(gastSelected);
+		gastSelected = null;
+	}
+
+	@Command
+	public void doGuestSearch() {
+		gastList.clear();
+		GastDao gastDao = ServiceLocator.getGastDao();
+		System.out.println(gastSuche);
+		gastList.addAll(gastDao.getAllFilteredUser(gastSuche));
+		gastSelected = null;
+	}
+
+	@Command
+	public void doNew() {
+		gastSelected = new Gast();
+	}
+
+	@Command
+	public void doSave() {
+		GastDao gastDao = ServiceLocator.getGastDao();
+		gastDao.saveOrUpdate(gastSelected);
+		if (!gastList.contains(gastSelected))
+			gastList.add(gastSelected);
+	}
+
+	@DependsOn("datumVon")
+	public LocalDate getDatumBis() {
+		if (datumBis.isBefore(datumVon)
+				|| datumBis.isAfter(datumVon.plusMonths(MAX_BUCHUNGSLAENGE))) {
+			datumBis = datumVon;
+		}
+		return datumBis;
+	}
+
+	@DependsOn("datumVon")
 	public SimpleDateConstraint getDatumBisConstraint() {
 		return new SimpleDateConstraint("between "
 				+ datumVon.toString("yyyMMdd") + " and "
@@ -42,52 +77,12 @@ public class IndexCtrl {
 		return datumVon;
 	}
 
-	@NotifyChange({ "zimmerZeitraumBelegungList",
-			"zimmerZeitraumBelegungSelected", "datumBis", "datumBisConstraint" })
-	public void setDatumVon(LocalDate datumVon) {
-		this.datumVon = datumVon;
+	public Gast getGastSelected() {
+		return gastSelected;
 	}
 
-	public LocalDate getDatumBis() {
-		if (datumBis.isBefore(datumVon)
-				|| datumBis.isAfter(datumVon.plusMonths(MAX_BUCHUNGSLAENGE))) {
-			datumBis = datumVon;
-		}
-		return datumBis;
-	}
-
-	@NotifyChange({ "zimmerZeitraumBelegungList",
-			"zimmerZeitraumBelegungSelected" })
-	public void setDatumBis(LocalDate datumBis) {
-		this.datumBis = datumBis;
-	}
-
-	public ZimmerZeitraumBelegung getZimmerZeitraumBelegungSelected() {
-		return zimmerZeitraumBelegungSelected;
-	}
-
-	public void setZimmerZeitraumBelegungSelected(
-			ZimmerZeitraumBelegung zimmerZeitraumBelegungSelected) {
-		this.zimmerZeitraumBelegungSelected = zimmerZeitraumBelegungSelected;
-		System.out.println(zimmerZeitraumBelegungSelected.getZimmer());
-	}
-
-	@NotifyChange("zimmerZeitraumBelegungSelected")
-	public ListModel<ZimmerZeitraumBelegung> getZimmerZeitraumBelegungList() {
-		zimmerZeitraumBelegungSelected = null;
-		if (zimmerZeitraumBelegungList == null) {
-			zimmerZeitraumBelegungList = new ListModelList<ZimmerZeitraumBelegung>();
-		} else {
-			zimmerZeitraumBelegungList.clear();
-		}
-		ZimmerZeitraumBelegungDto zimmerZeitraumBelegungDto = ServiceLocator
-				.getZimmerZeitraumBelegungDto();
-		zimmerZeitraumBelegungList.addAll(zimmerZeitraumBelegungDto.getAll(
-				datumVon, datumBis));
-		for (ZimmerZeitraumBelegung element : zimmerZeitraumBelegungList) {
-			System.out.println(element.getZimmer());
-		}
-		return zimmerZeitraumBelegungList;
+	public String getGastSuche() {
+		return gastSuche;
 	}
 
 	public ListModel<Gast> getItems() {
@@ -99,46 +94,56 @@ public class IndexCtrl {
 		return gastList;
 	}
 
-	@Command
-	@NotifyChange({ "gastSuche", "gastSelected", "gastList" })
-	public void doGuestSearch() {
-		gastList.clear();
-		GastDao gastDao = ServiceLocator.getGastDao();
-		System.out.println(gastSuche);
-		gastList.addAll(gastDao.getAllFilteredUser(gastSuche));
-		gastSelected = null;
+	@DependsOn({ "datumVon", "datumBis" })
+	public ListModel<ZimmerZeitraumBelegung> getZimmerZeitraumBelegungList() {
+		zimmerZeitraumBelegungSelected = null;
+		if (zimmerZeitraumBelegungList == null) {
+			zimmerZeitraumBelegungList = new ListModelList<ZimmerZeitraumBelegung>();
+		} else {
+			zimmerZeitraumBelegungList.clear();
+		}
+		ZimmerZeitraumBelegungDto zimmerZeitraumBelegungDto = ServiceLocator
+				.getZimmerZeitraumBelegungDto();
+		zimmerZeitraumBelegungList.addAll(zimmerZeitraumBelegungDto.getAll(
+				datumVon, datumBis));
+		return zimmerZeitraumBelegungList;
+	}
+
+	@DependsOn({ "datumVon", "datumBis" })
+	public ZimmerZeitraumBelegung getZimmerZeitraumBelegungSelected() {
+		return zimmerZeitraumBelegungSelected;
+	}
+
+	@DependsOn({ "datumVon", "datumBis", "zimmerZeitraumBelegungSelected", "gastSelected" })
+	public boolean isZusammenfassungAnzeigen() {
+		if (datumVon == null || datumBis == null || zimmerZeitraumBelegungSelected == null || gastSelected == null) {
+			return false;
+		}
+		if (zimmerZeitraumBelegungSelected.getStatus() != Status.FREI) {
+			return false;
+		}
+		return true;
+	}
+
+	public void setDatumBis(LocalDate datumBis) {
+		this.datumBis = datumBis;
+	}
+
+	public void setDatumVon(LocalDate datumVon) {
+		this.datumVon = datumVon;
 	}
 
 	public void setGastSelected(Gast gastSelected) {
 		this.gastSelected = gastSelected;
 	}
 
-	public Gast getGastSelected() {
-		return gastSelected;
+	public void setGastSuche(String gastSuche) {
+		this.gastSuche = gastSuche;
 	}
 
-	@Command
-	@NotifyChange({ "gastSelected", "gastList" })
-	public void doNew() {
-		gastSelected = new Gast();
-	}
-
-	@Command
-	@NotifyChange({ "gastSelected", "gastList" })
-	public void doSave() {
-		GastDao gastDao = ServiceLocator.getGastDao();
-		gastDao.saveOrUpdate(gastSelected);
-		if (!gastList.contains(gastSelected))
-			gastList.add(gastSelected);
-	}
-
-	@Command
-	@NotifyChange({ "gastSelected", "gastList" })
-	public void doDelete() {
-		GastDao gastDao = ServiceLocator.getGastDao();
-		gastDao.delete(gastSelected);
-		gastList.remove(gastSelected);
-		gastSelected = null;
+	public void setZimmerZeitraumBelegungSelected(
+			ZimmerZeitraumBelegung zimmerZeitraumBelegungSelected) {
+		this.zimmerZeitraumBelegungSelected = zimmerZeitraumBelegungSelected;
 	}
 
 }
