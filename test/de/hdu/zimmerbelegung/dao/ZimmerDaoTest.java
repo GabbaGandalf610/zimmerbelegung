19672410d2ff0a54a446a2b2417ac74aa2dbd9a5
Stefan Feilmeier <Stefan Feilmeier@9f5d0959-232f-0424-817a-97dd6493bbc2>
2012-05-16 19:33:51 +0000

    Aufräumen der Hibernate-Spring-Konfiguration: - zentrale Bean-Konfiguration (beans.xml) - datenbankspezifische Konfiguration (produktiv, entwicklung, test) - automatische Wahl der Datenbank (MySQL oder MSSQL)

---
+++ test/de/hdu/zimmerbelegung/dao/ZimmerDaoTest.java
@@ -0,0 +1,49 @@
+package de.hdu.zimmerbelegung.dao;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import de.hdu.zimmerbelegung.model.Zimmer;
+
+public class ZimmerDaoTest extends AbstractDataAccessTest {
+	private ZimmerDao zimmerDao;
+	private String tableName = "Zimmer";
+
+	public void setZimmerDao(ZimmerDao zimmerDao) {
+		this.zimmerDao = zimmerDao;
+	}
+
+	@SuppressWarnings("deprecation")
+	@Test
+	public void testFindById() {
+		String[] tables = { tableName };
+		// delete all rows from db table
+		deleteFromTables(tables);
+		// create new line in table
+		zimmerDao.saveOrUpdate(new Zimmer(1, "Beschreibung", 12.23f));
+		zimmerDao.saveOrUpdate(new Zimmer(2, "Beschreibung2", 23.34f));
+		List<Zimmer> alleZimmer = zimmerDao.getAll();
+		for (Zimmer z : alleZimmer) {
+			int id = z.getId();
+			Zimmer found = zimmerDao.getId(id);
+			assertEquals(z.getZimmerbeschreibung(),
+					found.getZimmerbeschreibung());
+		}
+		// delete all rows from db table
+		deleteFromTables(tables);
+	}
+
+	@Test
+	public void testFindAll() {
+		// already tested by testFindById()
+		testFindById();
+	}
+
+	@Test
+	public void testCreate() {
+		// already tested by testFindById()
+		testFindById();
+	}
+
+}
