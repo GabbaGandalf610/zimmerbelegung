a89c3486f802a49e75a2d2a2dbb4376bf115385e
wagnerfranz12 <wagnerfranz12@googlemail.com>
2012-05-16 17:23:09 +0000

    Test mit JUnit begonnen

---
+++ test/de/hdu/zimmerbelegung/dao/GastDaoTest.java
@@ -0,0 +1,188 @@
+package de.hdu.zimmerbelegung.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.zkoss.zul.ListModelList;
+
+import de.hdu.zimmerbelegung.ctrl.AdminGastCtrl;
+import de.hdu.zimmerbelegung.model.Gast;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations={"/test-application-context.xml"})
+//@ContextConfiguration({"../spring-hibernate.test.xml","../hibenate.cfg.test.xml"})
+public class GastDaoTest extends AbstractDataAccessTest{
+
+	private AdminGastCtrl controller;
+	GastDao gast;
+//	
+//	@Before
+//	public void setUp() throws Exception {
+//		gast = new GastDao();
+//	}
+
+	@Test
+	public void test() {
+//		ListModelList<Gast> gastList;
+//		gastList = (ListModelList<Gast>) controller.getItems();
+//		Gast testgast = gast.get(0);
+//		gast.saveOrUpdate(new Gast(1,"Franz","Wagner","Kapellenstraße 66","94527","Aholming","Deutschland","09931","0160","09931","a.b@c.de","wichtiger Kommentar"));
+//		fail("Not yet implemented");
+		System.out.println("test erfolgreich");
+	}
+	
+//	
+//	@Test
+//	public void testGet() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSaveOrUpdate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDelete() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetAll() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCheckDaoConfig() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testHibernateDaoSupport() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetSessionFactory() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testCreateHibernateTemplate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetSessionFactory() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testSetHibernateTemplate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetHibernateTemplate() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetSession() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetSessionBoolean() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testConvertHibernateAccessException() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testReleaseSession() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testDaoSupport() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testAfterPropertiesSet() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testInitDao() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testObject() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testGetClass() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testHashCode() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testEquals() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testClone() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testToString() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testNotify() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testNotifyAll() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testWaitLong() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testWaitLongInt() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testWait() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	public void testFinalize() {
+//		fail("Not yet implemented");
+//	}
+
+}
