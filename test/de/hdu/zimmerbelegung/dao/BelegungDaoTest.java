aacbc5ba3037f7df40b6dd808f36f0687baa63bc
wagnerfranz12 <wagnerfranz12@googlemail.com>
2012-06-04 19:59:20 +0000

    Erweiterung der Dao-Tests

--- test/de/hdu/zimmerbelegung/dao/BelegungDaoTest.java
+++ test/de/hdu/zimmerbelegung/dao/BelegungDaoTest.java
@@ -26,10 +26,10 @@ import de.hdu.zimmerbelegung.model.Zimmer;
 public class BelegungDaoTest extends AbstractDataAccessTest {
 
 	private BelegungDao belegungDao;
-	private BelegungKopfDao belegungKopfDao;
 	private GastDao gastDao;
-	private String tableName = "Belegung";
 	private ZimmerDao zimmerDao;
+	private BelegungKopfDao belegungKopfDao;
+	private String tableName = "Belegung";
 
 	/**
 	 * Gets the {@link BelegungDao} object injected form the bean belegungDaoTest.
@@ -37,14 +37,6 @@ public class BelegungDaoTest extends AbstractDataAccessTest {
 	 */
 	public void setBelegungDao(BelegungDao belegungDao) {
 		this.belegungDao = belegungDao;
-	}
-
-	/**
-	 * Gets the {@link BelegungKopfDao} object injected form the bean belegungDaoTest.
-	 * @param belegungDao a {@link BelegungKopfDao} object that injects the corresponding bean.
-	 */
-	public void setBelegungKopfDao(BelegungKopfDao belegungKopfDao) {
-		this.belegungKopfDao = belegungKopfDao;
 	}
 
 	/**
@@ -64,23 +56,21 @@ public class BelegungDaoTest extends AbstractDataAccessTest {
 	}
 
 	/**
-	 * testFindAll is already included in testFindById
+	 * Gets the {@link BelegungKopfDao} object injected form the bean belegungDaoTest.
+	 * @param belegungDao a {@link BelegungKopfDao} object that injects the corresponding bean.
 	 */
-	@Test
-	public void testCreate() {
-		// already tested by testFindById()
-		testFindById();
-	}
-	
-	/**
-	 * testFindAll is already included in testFindById
-	 */
-	@Test
-	public void testFindAll() {
-		// already tested by testFindById()
-		testFindById();
+	public void setBelegungKopfDao(BelegungKopfDao belegungKopfDao) {
+		this.belegungKopfDao = belegungKopfDao;
 	}
 
+	/**
+	 * Simple test that checks if the test class is up and running
+	 */
+	@Test
+	public void tests(){
+		System.out.println("Hallo");
+	}
+	
 	/**
 	 * testFindById is a test case that tests all the basic functions of the Dao, new object, update, get and delete
 	 */
@@ -114,11 +104,142 @@ public class BelegungDaoTest extends AbstractDataAccessTest {
 	}
 
 	/**
-	 * Simple test that checks if the test class is up and running
+	 * testFindAll is already included in testFindById
 	 */
 	@Test
-	public void tests(){
-		System.out.println("Hallo");
+	public void testFindAll() {
+		// already tested by testFindById()
+		testFindById();
 	}
 
+	/**
+	 * testFindAll is already included in testFindById
+	 */
+	@Test
+	public void testCreate() {
+		// already tested by testFindById()
+		testFindById();
+	}
+	
+	
+	/**
+	 * testAllInZeitraum tests the Dao method getAllinZeitraum
+	 */
+	@Test
+	public void testAllInZeitraum(){
+		LocalDate datumBis = LocalDate.now();
+		Gast gastTest = new Gast("Hans", "Dampf", "Musterstraße", "94555",
+				"Musterstadt", "Deutschland", "08541", "0160", "08541", "f.g@h.ij",
+				"etwas längerer Kommentar");
+		gastDao.saveOrUpdate(gastTest);
+		Zimmer zimmerTest = new Zimmer(1, "Beschreibung", 12.23f);
+		zimmerDao.saveOrUpdate(zimmerTest);
+		BelegungKopf belegungKopfTest = new BelegungKopf(gastTest);
+		belegungKopfDao.saveOrUpdate(belegungKopfTest);
+		Belegung belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				LocalDate.now(), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(1), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(2), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(3), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		
+		List<Belegung> alleBelegung = belegungDao.getAll();
+		assertEquals("Anzahl der Belegungen: ", 4, alleBelegung.size());
+
+		alleBelegung = belegungDao.getAllInZeitraum(LocalDate.now(), datumBis.plusDays(2));
+		assertEquals("Anzahl der Belegungen: ", 3, alleBelegung.size());
+	}
+	
+	/**
+	 * testDelete Test, if a object can be deleted
+	 */
+	@Test
+	public void testDelete(){
+		LocalDate datumBis = LocalDate.now();
+		Gast gastTest = new Gast("Hans", "Dampf", "Musterstraße", "94555",
+				"Musterstadt", "Deutschland", "08541", "0160", "08541", "f.g@h.ij",
+				"etwas längerer Kommentar");
+		gastDao.saveOrUpdate(gastTest);
+		Zimmer zimmerTest = new Zimmer(1, "Beschreibung", 12.23f);
+		zimmerDao.saveOrUpdate(zimmerTest);
+		BelegungKopf belegungKopfTest = new BelegungKopf(gastTest);
+		belegungKopfDao.saveOrUpdate(belegungKopfTest);
+		Belegung belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				LocalDate.now(), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(1), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(2), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(3), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		
+		List<Belegung> alleBelegung = belegungDao.getAll();
+		
+		for (Belegung z : alleBelegung) {
+			belegungDao.delete(z);
+		}
+		
+		alleBelegung = belegungDao.getAll();
+		assertEquals("Die Tabelle darf keine Zeile enthalten", 0, alleBelegung.size());
+	}
+	
+	@Test
+	public void testDeleteAll(){
+		LocalDate datumBis = LocalDate.now();
+		Gast gastTest = new Gast("Hans", "Dampf", "Musterstraße", "94555",
+				"Musterstadt", "Deutschland", "08541", "0160", "08541", "f.g@h.ij",
+				"etwas längerer Kommentar");
+		gastDao.saveOrUpdate(gastTest);
+		Zimmer zimmerTest = new Zimmer(1, "Beschreibung", 12.23f);
+		zimmerDao.saveOrUpdate(zimmerTest);
+		BelegungKopf belegungKopfTest = new BelegungKopf(gastTest);
+		belegungKopfDao.saveOrUpdate(belegungKopfTest);
+		Belegung belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				LocalDate.now(), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(1), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(2), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		belegungTest = new Belegung(BelegungArt.BUCHUNG,
+				datumBis.plusDays(3), zimmerTest, gastTest, belegungKopfTest);
+		belegungDao.save(belegungTest);
+		
+		List<Belegung> alleBelegung = belegungDao.getAll();
+		
+		belegungDao.deleteAll();
+		
+		alleBelegung = belegungDao.getAll();
+		assertEquals("Die Tabelle darf keine Zeile enthalten", 0, alleBelegung.size());
+	}
+
+	
+	@Test
+	public void testbelegungKopfCreate(){
+		LocalDate datumBis = LocalDate.now();
+		Gast gastTest = new Gast("Hans", "Dampf", "Musterstraße", "94555",
+				"Musterstadt", "Deutschland", "08541", "0160", "08541", "f.g@h.ij",
+				"etwas längerer Kommentar");
+		gastDao.saveOrUpdate(gastTest);
+		Zimmer zimmerTest = new Zimmer(1, "Beschreibung", 12.23f);
+		zimmerDao.saveOrUpdate(zimmerTest);
+		
+		belegungKopfDao.create(BelegungArt.RESERVIERUNG, LocalDate.now(), datumBis.plusDays(2), zimmerTest, gastTest);
+		
+		List<BelegungKopf> alleBelegungKopf = belegungKopfDao.getAll();
+		
+		assertEquals("Es soll 1 BelegungKopf vorhanden sein" , 1, alleBelegungKopf.size());
+	}
 }
